4b24b304a9b186c88ef73c1f7270640d3433d66f
myj03qq-com <292097233+myj03qq-com@users.noreply.github.com>
2026-06-24 21:04:10 +0800

    添加中国旅游攻略页面

---
+++ sw.js
@@ -0,0 +1,27 @@
+var CACHE = 'travel-guide-v1';
+var FILES = [
+  './中国旅游攻略.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(FILES);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      return r || fetch(e.request).then(function(resp) {
+        if (resp.ok) {
+          var clone = resp.clone();
+          caches.open(CACHE).then(function(c) { c.put(e.request, clone); });
+        }
+        return resp;
+      });
+    })
+  );
+});
